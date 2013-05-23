1794888806ec7cb2bc13229fc11bd5c80d647efe
Danny Chen <aznlucidx@gmail.com>
2013-05-22 20:00:36 -0700

    Made the form and initial login api code

--- chenzen/views/loginapi.py
+++ chenzen/views/loginapi.py
@@ -1,15 +1,29 @@
 from chenzen import chenzen
 from chenzen.models import Users
+from chenzen.forms import LoginForm
 from flask.views import MethodView
 from flask import request
+from flask import redirect
+from flask import render_template
+from flask.ext.login import login_use
+from flask.ext.login import login_required
+from flask.ext.login import logout_user
+from flask.ext.login import current_user
+
 import json
 
 
 class LoginAPI(MethodView):
 
+login_manager = 
     def get(self, id):
-        if id is None:
-        	users = Users.query.all()
-            pass
-        else:
-            pass
+       form = LoginForm()
+       if form.validate_on_submit():
+       		try:
+       			user = User.objects.get(username=request.form['username'])
+
+       			#connect to db and query username and password....
+
+       			login_user(user)
+       			flash('Logged In', 'success')
+
